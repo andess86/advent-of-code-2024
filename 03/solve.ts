764006fa670a44b486ef9439887621039f081f30
Anders Svensson <andess86@gmail.com>
2024-12-03 14:10:24 +0100

    beautiful

--- 03/solve.ts
+++ 03/solve.ts
@@ -3,12 +3,9 @@ const data = await input.text();
 
 const pattern = /mul\((\d{1,3}),(\d{1,3})\)/g;
 
-const total = [...data.matchAll(pattern)].reduce((sum, match) => {
-  const first = Number(match[1]);
-  const second = Number(match[2]);
-  const multiplied = first * second;
-
-  return sum + multiplied;
-}, 0);
+const total = [...data.matchAll(pattern)].reduce(
+  (sum, match) => sum + Number(match[1]) * Number(match[2]),
+  0
+);
 
 console.log(total);
