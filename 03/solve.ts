0def369e9adc665c89a99df58f24af2c79e29fdd
Anders Svensson <andess86@gmail.com>
2024-12-05 08:42:12 +0100

    day 3 part 2

--- 03/solve.ts
+++ 03/solve.ts
@@ -3,9 +3,31 @@ const data = await input.text();
 
 const pattern = /mul\((\d{1,3}),(\d{1,3})\)/g;
 
-const total = [...data.matchAll(pattern)].reduce(
+const totalPart1 = [...data.matchAll(pattern)].reduce(
   (sum, match) => sum + Number(match[1]) * Number(match[2]),
   0
 );
 
-console.log(total);
+const solvePart2 = () => {
+  const pattern = /do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)/g;
+  let enabled = true;
+  const total = [...data.matchAll(pattern)].reduce((sum, match) => {
+    if (match[0] === "don't()") {
+      enabled = false;
+      return sum;
+    }
+    if (match[0] === "do()") {
+      enabled = true;
+      return sum;
+    }
+    if (enabled) {
+      sum = sum + Number(match[1]) * Number(match[2]);
+    }
+    return sum;
+  }, 0);
+
+  return total;
+};
+
+console.log(totalPart1);
+console.log(solvePart2());
